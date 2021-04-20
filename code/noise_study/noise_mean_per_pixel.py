46781903ac255b1eadabb96214d9a2d81cc91d4a
Max Vos <max.vos7@gmail.com>
2021-04-20 10:41:30 +0200

    Update noise_mean_per_pixel.py

--- code/noise_study/noise_mean_per_pixel.py
+++ code/noise_study/noise_mean_per_pixel.py
@@ -23,38 +23,58 @@ def fetch_noise_mean_array(trim_level: str) -> np.array:
     return np.array(np.loadtxt(file, delimiter=',', dtype=int))
 
 
-def getNoiseMeanPrediction(trim0Array: np.array, trimFArray: np.array, row: int, col: int, trimLevel: str) -> int:
+def getNoiseMeanPrediction(decTrimArray: list, trimMatrixArray: list, row: int, col: int, trimLevel: str) -> int:
     decTrimLevel = int(trimLevel, 16)
 
-    trim0Value = trim0Array[row][col]
-    trimFValue = trimFArray[row][col]
-    return (trimFValue - trim0Value) * (decTrimLevel / 15) + trim0Value
+    # [0: 1200, 15: 1800, 4: 1500]
+    trimDataArray = list(map(lambda trimMatrix: trimMatrix[row][col], trimMatrixArray))
+    poly = np.poly1d(np.polyfit(decTrimArray, trimDataArray, 1))
+
+    return poly(decTrimLevel)
 
 
-def makeDistanceToPredictedArray(trim0Array: np.array, trimFArray: np.array, trimToPredictArray: np.array,
-                                 toPredictTrim: str) -> np.array:
-    width = trimToPredictArray.shape[0]
-    height = trimToPredictArray.shape[1]
-
-    distanceToPredictedArray = []
-    """"
-    This method makes an array with the distance to the predicted value per pixel.
+def makeDistanceToPredictedArray(trimArray: list, trimDataArray: list, trimToPredictArray: np.array,
+                                 toPredictTrim: str) -> list:
     """
+    This method makes an array with the distance to the predicted value per pixel.
+
+    :param trimArray: Give an array of trim levels, to be used for prediction (in same order as trimDataArray)
+    :param trimDataArray: Give for each trim level, the matrix with means per pixel.
+    :param trimToPredictArray: Give the trim matrix to predict.
+    :param toPredictTrim: Give the trim level that is the to be predicted (trim level of the trimToPredictArray)
+    :return: An 1D array of distance to predicted value.
+    """
+    distanceToPredictedArray = []
+    decTrimArray = list(map(lambda trim: int(trim, 16), trimArray))
     for i in range(trimToPredictArray.shape[0]):
         for j in range(trimToPredictArray.shape[1]):
-            distanceToPredictedArray.append(trimToPredictArray[i][j] - getNoiseMeanPrediction(trim0Array, trimFArray,
-                                                                        i, j, toPredictTrim))
+            if trimToPredictArray[i][j] > 0 and trim0Array[i][j] > 0 and trimFArray[i][j] > 0:
+                continue
+            else:
+                noiseMeanPrediction = getNoiseMeanPrediction(decTrimArray, trimDataArray, i, j, toPredictTrim)
+                distanceToPredictedArray.append(
+                    trimToPredictArray[i][j] - noiseMeanPrediction)
 
     return distanceToPredictedArray
 
 
+def showHistogram(predictionOffsetArray: list):
+    hist_data = np.histogram(predictionOffsetArray,
+                             np.arange(min(predictionOffsetArray), max(predictionOffsetArray), 1))
+    logs = hist_data[0].astype(float)
+    plt.semilogy(hist_data[1][:-1], logs, marker='.', linestyle='')
+    plt.suptitle("Use trim 0, D, F to predict trim D")
+    plt.xlabel("Difference of prediction and real value (current)")
+    plt.ylabel("The amount of times difference is found")
+    plt.show()
+
+
 trim0Array = fetch_noise_mean_array('0')
 trimFArray = fetch_noise_mean_array('F')
+trim1Array = fetch_noise_mean_array('1')
+
 trimDArray = fetch_noise_mean_array('D')
 
-predictionOffset = makeDistanceToPredictedArray(trim0Array, trimFArray, trimDArray, 'D')
+predictionOffset = makeDistanceToPredictedArray(['0', 'F'], [trim0Array, trimFArray], trimDArray, 'D')
 
-hist_data = np.histogram(predictionOffset, np.arange(-10, 1, 0.1))
-logs = hist_data[0].astype(float)
-plt.plot(hist_data[1][:-1], logs, marker='.', linestyle='', linewidth=2)
-plt.show()
+showHistogram(predictionOffset)
