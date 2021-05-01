3a820449810f79dffe56bb2dc9b9be14c2aed898
Max Vos <max.vos7@gmail.com>
2021-05-01 10:02:58 +0200

    fix: counts

--- code/noise_study/study_masks.py
+++ code/noise_study/study_masks.py
@@ -14,25 +14,6 @@ def make_mask_map(predictions_to_map: list, mask_type: str = None):
         (256, 256, 4))  # create a 256x256x3 array where each pixel has an RGB value of 1-bit representation
 
     patches = []
-    if mask_type != None:
-        outside_std_mask_matrices = list(map(
-            lambda prediction_trim: mask_handler.get_mask_matrices(prediction_trim, [mask_type])[0], predictions_to_map)
-        )
-        outside_2std_total = np.zeros(outside_std_mask_matrices[0].shape)
-        for matrix in outside_std_mask_matrices:
-            outside_2std_total += matrix
-        cmap = cm.get_cmap('summer_r', len(predictions_to_map) + 1)
-        for i in np.arange(len(predictions_to_map) + 1):
-            bitmap[outside_2std_total == i] = cmap(i)
-        mpatches.Patch(color=cmap(0),
-                       label=f"Inside 2 std: {np.count_nonzero(outside_2std_total == 0)}")
-        for i in np.arange(1, len(predictions_to_map) + 1):
-            patches.append(mpatches.Patch(
-                color=cmap(i),
-                label=f"Outside 2 std in {i} prediction{'s' if i > 1 else ''}: {np.count_nonzero(outside_2std_total == i)}")
-            )
-        patches.append(mpatches.Patch(color='white',
-                                      label=f"Tot Outside 2 standard deviations: {np.count_nonzero(outside_2std_total)}"))
 
     # Adding to bitmap the dead pixels.
     dead_matrices = list(map(
@@ -45,7 +26,28 @@ def make_mask_map(predictions_to_map: list, mask_type: str = None):
     bitmap[dead_total > 0] = matplotlib.colors.to_rgba('black')
     patches.append(mpatches.Patch(color='black', label=f"Dead: {np.count_nonzero(dead_total)}"))
 
-    plt.legend(bbox_to_anchor=(1.042, 0.8), loc=2, borderaxespad=0, handles=patches,
+    if mask_type != None:
+        outside_std_mask_matrices = list(map(
+            lambda prediction_trim: mask_handler.get_mask_matrices(prediction_trim, [mask_type])[0], predictions_to_map)
+        )
+        outside_2std_total = np.zeros(outside_std_mask_matrices[0].shape)
+        for matrix in outside_std_mask_matrices:
+            outside_2std_total += matrix
+        cmap = cm.get_cmap('summer_r', len(predictions_to_map) + 1)
+        good_bevaving_count = np.count_nonzero(outside_2std_total == 0) - np.count_nonzero(dead_total)
+        patches.append(mpatches.Patch(color=cmap(0),
+                                      label=f"Inside 4 standard deviations: {good_bevaving_count}")
+                       )
+        for i in np.arange(1, len(predictions_to_map) + 1):
+            bitmap[outside_2std_total == i] = cmap(i)
+            patches.append(mpatches.Patch(
+                color=cmap(i),
+                label=f"Outside 2 std in {i} prediction{'s' if i > 1 else ''}: {np.count_nonzero(outside_2std_total == i)}")
+            )
+        patches.append(mpatches.Patch(color='white',
+                                      label=f"Tot Outside 4 standard deviations: {np.count_nonzero(outside_2std_total)}"))
+
+    plt.legend(bbox_to_anchor=(1.042, 1), loc=2, borderaxespad=0, handles=patches,
                frameon=False, handlelength=1.25)
     plt.axis([-2, 257, -2, 257])  # to better show the borders
     plt.xlabel("Pixel x coordinate")
@@ -54,4 +56,4 @@ def make_mask_map(predictions_to_map: list, mask_type: str = None):
     plt.show()
 
 
-make_mask_map(list(map(lambda value: format(value, 'x').upper(), np.arange(1, 15))), 'Outside_5Std_Not_Dead')
+make_mask_map(list(map(lambda value: format(value, 'x').upper(), np.arange(1, 15))), 'Outside_4Std_Not_Dead')
